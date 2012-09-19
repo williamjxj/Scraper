75b55919f950eab6e3b426185963cfee3b66ec71
U-Home\William <William@Home.(none)>
2012-09-19 14:01:29 -0700

    update from server at Wed, Sep 19, 2012  2:01:28 PM

--- lib/yahoo.pm
+++ lib/yahoo.pm
@@ -5,19 +5,16 @@ use common;
 @ISA = qw(common);
 use strict;
 our ( $sth );
-use constant CONTACTS => q{contexts};
 
 sub new {
 	my ( $type, $dbh_handle ) = @_;
 	my $self = {};
 	$self->{dbh} = $dbh_handle;
-	$self->{app} = 'food';
-	$self->{uniq_links} = [];
 	bless $self, $type;
 }
 
 # use for yy.pl. yahoo.com can pagnation, google.com can't.
-sub strip_yahoo
+sub strip_result
 {
 	my ( $self, $html ) = @_;
 
@@ -30,25 +27,53 @@ sub strip_yahoo
 	return $1;
 }
 
-sub parse_yahoo_page
+sub parse_result
 {
     my ( $self, $html ) = @_;
     return unless $html;
-	$html =~ m {
-        <strong>
-		(\d+)
-        </strong>
-		\s*
-        <a\s
+    my $aoh = [];    
+	while ($html =~ m {
+		<div\sclass=(?:"res"|res)>
 		(?:.*?)
-		href="(.*?)"
+		<h3>
+		(?:.*?)
+		href="
+		(.*?)	#1.链接地址
+		"
 		(?:.*?)
 		>
-		(\d+)
+		(.*?)	#2.标题
         </a>
-    }sgix;
-	my ($cur_page, $alink, $next_page) = ($1, $2, $3);
-	return [ $cur_page, $alink, $next_page ];
+        (?:.*?)
+        <div\sclass="abstr">
+        (.*?)	#3.正文
+        </div>
+    }sgix) {
+        my ($t1,$t2,$t3) = ($1,$2,$3);
+		$t1 =~ s/^\/url\?q=//  if($t1 =~ m/^\/url/);
+		$t1 =~ s/\&sa=.*$//  if($t1 =~ m/\&sa=/);
+		$t2 =~ s/\<em>//g if ($t2=~m/\<em>/);
+		$t2 =~ s/\<\/em>//g if ($t2=~m/\<\/em>/);
+		$t2 =~ s/\<b>\.+\<\/b>//s;
+		$t3 =~ s/\<br>.*$//sg;
+		$t3 =~ s/\<b>\.\.\.\<\/b>/.../sg;
+		$t3 =~ s/\<.*?>//sg;
+        push (@{$aoh}, [$t1,$t2,$t3]);
+    }
+    return $aoh;
+}
+
+
+# 相关搜索。
+sub strip_related_keywords
+{
+	my ( $self, $html ) = @_;
+	return $html;
+}
+sub get_related_keywords
+{
+	my ( $self, $html ) = @_;
+	return $html;
 }
 
 1;
