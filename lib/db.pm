a37756260c7734b33dcb5d20721ea6b3e7c254e7
U-Home\William <William@Home.(none)>
2012-09-20 00:00:46 -0700

    update from server at Thu, Sep 20, 2012 12:00:45 AM

--- lib/db.pm
+++ lib/db.pm
@@ -4,6 +4,7 @@ use Data::Dumper;
 use strict;
 use utf8;
 
+# 这里,没有返回类目标, 而是返回它的一个属性句柄, 所以没有办法继承.
 sub new
 {
     my ($type, $user, $pass, $dsn) = @_;
@@ -15,73 +16,6 @@ sub new
 	# mysql_query("SET CHARACTER_SET_RESULTS='UTF8'");
 	bless $self, $type;
 	return $self->{dbh};
-}
-
-# Thu 25 Mar
-sub get_end_date {
-	my ( $self, $todate ) = @_;
-	my $sth =
-	  $self->{dbh}->prepare( qq{ select date_format(date_sub(now(), interval } 
-		  . $todate
-		  . qq{ day), '%a %b %d' ) } );
-	$sth->execute();
-	my @row = $sth->fetchrow_array();
-	$sth->finish();
-	return $row[0];
-}
-
-# trace back to 2 days: $todate=2
-sub get_routine_date
-{
-	my ($self,$todate) = @_;
-
-	$todate = $self->{dbh}->quote($todate);
-	my $sth = $self->{dbh}->prepare(qq{ select date_format(date_sub(now(), interval $todate  day), '%d-%b-%y') });
-	$sth->execute();
-	my @row = $sth->fetchrow_array();
-	$sth->finish();
-	return $row[0];
-}
-
-
-sub get_baidu_rss {
-	my $self = shift;
-	my $sql = qq{ select name, url from baidu_rss };
-	$self->show_results($sql);
-}
-
-sub show_results
-{
-	my ($self, $sql) = @_;
-	my $count = 0;  # number of entries printed so far
-	my $total = 0;
-	my @label = (); # column label array
-	my $label_width = 0;
-
-	my $sth = $self->{dbh}->prepare ($sql);
-	$sth->execute ();
-
-	# get column names to use for labels and
-	# determine max column name width for formatting
-	@label = @{$sth->{NAME}};
-	foreach my $label (@label) {
-		$label_width = length ($label) if $label_width < length ($label);
-	}
-
-	print "Total columns for each record: [" . $sth->{NUM_OF_FIELDS} . "]\n\n";
-
-	while (my @ary = $sth->fetchrow_array ()) {
-		++ $total;
-		# print newline before 2nd and subsequent entries
-		print "\n" if ++$count > 1;
-		foreach (my $i = 0; $i < $sth->{NUM_OF_FIELDS}; $i++)
-		{
-			printf "%-*s", $label_width+1, $label[$i] . ":";
-			print " ", $ary[$i] if defined ($ary[$i]);
-			print "\n";
-		}
-	}
-	$sth->finish ();
 }
 
 1;
