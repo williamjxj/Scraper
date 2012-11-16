4cdc0cc91b8e74f1018fba05860d069a7f296d65
williamjxj <williamjxj@hotmail.com>
2012-11-15 22:07:51 -0700

    update from server at Thu Nov 15 22:07:51 MST 2012

--- threads/m1.pl
+++ threads/m1.pl
@@ -3,26 +3,19 @@
 use strict;
 use warnings;
 use utf8;
-#use encoding "gbk";
 use WWW::Mechanize;
-use CGI qw/:standard/;
 use Encode;
 
 use constant SURL => q{http://news.sogou.com/};
-
-print header(-charset=>'gbk');
-my $keyword = "中国";
-# my $keyword = "%D6%D0%B9%FA";
-
-#Encode::from_to($keyword, "utf8", "gbk");
-
-$keyword=encode ("gbk",decode("utf-8",$keyword));
 
 my $mech = WWW::Mechanize->new( ) or die;
 $mech->timeout( 20 );
 
 $mech->get( SURL );
 $mech->success or die $mech->response->status_line;
+
+my $keyword = "中国";
+#$keyword=encode("gbk", $keyword);
 
 $mech->submit_form(
     form_name => 'searchForm',
@@ -33,11 +26,5 @@ $mech->submit_form(
   }
 );
 $mech->success or die $mech->response->status_line;
-#print $mech->content;
 
-my $fh = FileHandle->new('../html/t6.html', "w" );
-binmode $fh, ':utf8';
-print $fh $mech->content;
-$fh->autoflush(1);
-$fh->close();
-
+print $mech->content;
