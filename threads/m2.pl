508ef9ba1b7ff9bbbc95cf3aebaed40647179a68
williamjxj <williamjxj@hotmail.com>
2012-11-15 22:34:25 -0700

    update from server at Thu Nov 15 22:34:25 MST 2012

--- threads/m2.pl
+++ threads/m2.pl
@@ -8,9 +8,9 @@ use Encode;
 
 my $url = q{http://news.sogou.com/news?p=42040301&mode=1&query=};
 
-my $keyword = "%D6%D0%B9%FA";
-#my $keyword = '中国';
-#$keyword=encode("gbk", $keyword);
+#my $keyword = "%D6%D0%B9%FA";
+my $keyword = '中国';
+$keyword=encode("gbk", $keyword);
 
 my $doc = get $url.$keyword;
 
